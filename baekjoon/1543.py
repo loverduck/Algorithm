79d87b15ddbf153e25ff4c7939c57b602799ed8a
Soyeon Yu <loverduck97@naver.com>
2020-10-15 20:35:56 +0900

    20201015

---
+++ baekjoon/1543.py
@@ -0,0 +1,15 @@
+# 문서 검색
+document = input()
+word = input()
+
+index = 0
+result = 0
+
+while len(document) - index >= len(word):
+    if document[index:index + len(word)] == word:
+        result += 1
+        index += len(word)
+    else:
+        index += 1
+
+print(result)
