f677a082d9cfd2f81e2ab24cd6f13003d7e40039
Soyeon Yu <loverduck97@naver.com>
2021-05-24 22:52:17 +0900

    20210524

---
+++ leetcode/77.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def combine(self, n: int, k: int) -> List[List[int]]:
+    result = []
+
+    def dfs(elements, start: int, k: int): 
+        if k == 0:
+            result.append(elements[:])
+        
+        for i in range(start, n + 1):
+            elements.append(i)
+            dfs(elements, i + 1, k - 1)
+            elements.pop()
+    
+    dfs([], 1, k)
+    return results
