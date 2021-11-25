18a23e1fa3fc726b1347dedb2a34f887eff3b31b
soyou <loverduck97@naver.com>
2021-11-25 23:24:11 +0900

    20211125

---
+++ programmers/징검다리 건너기.js
@@ -0,0 +1,19 @@
+function solution(stones, k) {
+  let count = -1;
+  let maxDist = 1;
+  while (maxDist <= k) {
+      count++;
+      let dist = 1;
+      for (let i = 0; i < stones.length; i++) {
+          if (stones[i] !== 0) {
+              maxDist = Math.max(maxDist, dist);
+              dist = 1;
+              stones[i]--;
+          } else {
+              dist++;
+          }
+      }
+  }
+  
+  return count;
+}
